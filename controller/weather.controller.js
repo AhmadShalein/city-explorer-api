e3b1d1e57c91f63e4adc743247ee42e8badb748f
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-06-17 09:35:40 +0300

    Update weather.controller.js

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -10,9 +10,9 @@ const Weather = require('../models/weather.model');
 const server = express(); // initialize your express server instance
 server.use(cors());
 
-const weatherHandle = (request, response) => {
-    let lat = request.quiry.lat;
-    let lon = request.quiry.lon;
+const weatherHandle = (request, response) =>{
+    let lat = request.query.lat;
+    let lon = request.query.lon;
     // let searchQuery = req.quiry.city_name;
     // let cityItem = weatherData.find (item => item.lat == lat && item.lon == lon && item.city_name.toLowerCase() == searchQuery.toLowerCase());
     // if (item != undefined)
@@ -24,17 +24,17 @@ const weatherHandle = (request, response) => {
     // {
     //     response.status(500).send('something went wrong');
     // }
-    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`).then(response => {
+    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`).then(res => {
         const arrOfDays = [];
-        response.data.data.map(item => {
+        res.data.data.map(item => {
         let descriptionDay = `Low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`;
-        let weatherDay = new Forecast(item.vaild_date,descriptionDay);
+        let weatherDay = new Weather(item.vaild_date,descriptionDay);
         arrOfDays.push(weatherDay);
     });
-    resopnse.send(arrOfDays);
+    response.send(arrOfDays);
     })
     .catch(error => {
-        resopnse.status(500).send(`Not found ${error}`);
+        response.status(500).send(`Not found ${error}`);
     });
 }
 
